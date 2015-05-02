96ba444a93e9f37aa98b26d61989cee961290d22
Stuart Farmer <email@stuartfarmer.me>
2015-05-02 10:56:50 -0400

    first commit

---
+++ Solvit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Solvit
+//
+//  Created by Stuart Farmer on 5/2/15.
+//  Copyright (c) 2015 Solvit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SolvitTests/SolvitTests.m
@@ -0,0 +1,40 @@
+//
+//  SolvitTests.m
+//  SolvitTests
+//
+//  Created by Stuart Farmer on 5/2/15.
+//  Copyright (c) 2015 Solvit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface SolvitTests : XCTestCase
+
+@end
+
+@implementation SolvitTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
